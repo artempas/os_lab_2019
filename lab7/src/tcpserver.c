aa3a0df4bd6c00f01b2f252b6b60ebdbd85dc45c
Artempas <Artempas@github.com>
2022-12-16 23:01:31 +0300

    lab 7

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,61 +2,213 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <getopt.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>	
+#include <sys/stat.h>
+#include <stdbool.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
+
+//#define SERV_PORT 10050
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
+int main(int argc, char *argv[]) {
   const size_t kSize = sizeof(struct sockaddr_in);
 
   int lfd, cfd;
   int nread;
-  char buf[BUFSIZE];
+
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
+	  pid_t child_pid = 0;
 
+  int pipeEnds[2];
+pipe(pipeEnds);
+
+
+
+  int SERV_PORT = -1;
+  int BUFSIZE = -1;
+  while (1) {
+    int current_optind = optind ? optind : 1;
+    static struct option options[] = {{"port", required_argument, 0, 0},
+                                      {"bufsize", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+    if (c == -1)
+        break;
+    switch (c) {
+        case 0: {
+            switch (option_index) {
+                case 0:
+                    SERV_PORT = atoi(optarg);
+                    if (!(SERV_PORT>0))
+                        return 0;
+                    break;
+                case 1:
+                    BUFSIZE = atoi(optarg);
+                    if (!(BUFSIZE>0))
+                        return 0;
+                    break;
+                default:
+                    printf("Index %d is out of options\n", option_index);
+            }
+        } break;
+        case '?':
+            printf("Unknown argument\n");
+            break;
+        default:
+            fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+  if (SERV_PORT == -1 || BUFSIZE == -1) {
+    fprintf(stderr, "Using: %s --port 20001 --bufsize 4\n", argv[0]);
+    return 1;
+  }
+
+  char buf[BUFSIZE];
+  printf("TCP-SERVER starts...\n");
+  //Возвращает файловый дескриптор(>=0), который будет использоваться как ссылка на созданный коммуникационный узел
+  //SOCK_STREAM для потоковых сокетов
   if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
+    perror("socket problem ");
     exit(1);
   }
 
+  //параметры для настройки адреса сокета
   memset(&servaddr, 0, kSize);
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(SERV_PORT);
 
+  //настройка адреса сокета
   if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
-    perror("bind");
+    perror("bind problem");
     exit(1);
   }
 
+  //Cообщает уровню протокола, что сокет готов к принятию новых входящих соединений
+  //Перевод сокета в пассивное (слушающее) состояние и создание очередей сокетов
   if (listen(lfd, 5) < 0) {
-    perror("listen");
+    perror("listen problem");
     exit(1);
   }
 
+  //Слушаем в цикле
   while (1) {
     unsigned int clilen = kSize;
 
+    //Является блокирующим – ожидает поступления запроса на соединение
     if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
-      perror("accept");
+      perror("accept problem ");
       exit(1);
     }
-    printf("connection established\n");
+    printf("connection established \n");
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
-    }
-
-    if (nread == -1) {
+     int num = 0;
+    if (read(cfd, &num, sizeof(int)) <= 0) {
       perror("read");
       exit(1);
     }
-    close(cfd);
+
+   
+
+    int SERV_PORT = ntohs(cliaddr.sin_port);
+    if (write(cfd, &SERV_PORT, sizeof(int)) < 0) {
+      perror("write");
+      exit(1);
+    }
+    child_pid = fork();
+    if (child_pid == 0) {
+      /////////////////////////// UDP code //////////////////
+      int sockfd, n;
+      char ipadr[16];
+      struct sockaddr_in servaddr1;
+      struct sockaddr_in cliaddr;
+      bool checklist[num + 1];
+      int all = 0;
+      for (int i = 0; i < num; i++)
+        checklist[i] = 0;
+
+
+      if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket problem");
+        exit(1);
+      }
+      
+      memset(&servaddr, 0, sizeof(struct sockaddr_in));
+      servaddr1.sin_family = AF_INET;
+      servaddr1.sin_addr.s_addr = htonl(INADDR_ANY);
+      servaddr1.sin_port = htons(SERV_PORT);
+
+
+      if (bind(sockfd, (SADDR *)&servaddr1, sizeof(struct sockaddr_in)) < 0) {
+        perror("bind problem");
+        exit(1);
+      }
+      printf("UDP-SERVER starts...\n");
+      int msg = 0;
+      //sleep(1);
+
+      while (true) {
+        unsigned int len = sizeof(struct sockaddr_in);
+        if ((n = recvfrom(sockfd, &msg, sizeof(msg), 0, (SADDR *)&cliaddr,
+                          &len)) < 0) {
+          perror("recvfrom");
+          exit(1);
+        }
+        if (msg != 0) {
+          checklist[msg] = 1;
+          printf("REQUEST %u      FROM %s : %d\n", msg,
+                 inet_ntop(AF_INET, &cliaddr.sin_addr.s_addr, ipadr, 16),
+                 ntohs(cliaddr.sin_port));
+        } else {
+          checklist[0] = 1;
+          int j;
+          for (j = 1; j < num+1; j++) {
+            if (checklist[j] != 1) {
+              write(pipeEnds[1], &j, sizeof(int));
+              checklist[0] = 0;
+            }
+          }
+          if (checklist[0] == 1) {
+            int k = -1;
+            write(pipeEnds[1], &k, sizeof(int));
+            break;
+          } else {
+            int k = 0;
+            write(pipeEnds[1], &k, sizeof(int));
+          }
+        }
+      }
+      /////////////////////////////////////////////////
+    }
+
+
+    int missing = 0;
+    while (true) {
+      read(pipeEnds[0], &missing, sizeof(int));
+       //printf("mi %u  \n", missing);
+      if (write(cfd, &missing, sizeof(missing)) < 0) {
+        perror("write problem");
+        exit(1);
+      }
+     
+    }
+    if(missing == -1)
+        kill(child_pid, SIGKILL);
   }
+
+    //Закрывает (или прерывает) все существующие соединения сокета
+    
+  
+    close(cfd);
+    return 0;
 }
--- lab7/src/udpclient.c
+++ lab7/src/udpclient.c
@@ -1,57 +1,161 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <arpa/inet.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <getopt.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
+//#define SERV_PORT 20001
+//#define BUFSIZE 1024
 #define SADDR struct sockaddr
 #define SLEN sizeof(struct sockaddr_in)
+#define SIZE sizeof(struct sockaddr_in)
 
 int main(int argc, char **argv) {
   int sockfd, n;
-  char sendline[BUFSIZE], recvline[BUFSIZE + 1];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
-  if (argc != 2) {
-    printf("usage: client <IPaddress of server>\n");
+  int SERV_PORT = -1;
+  int BUFSIZE = -1;
+  char IP[16] = {'\0'};
+
+  while (1) {
+    int current_optind = optind ? optind : 1;
+    static struct option options[] = {{"port", required_argument, 0, 0},
+                                      {"bufsize", required_argument, 0, 0},
+                                      {"ip",required_argument,0,0},
+                                      {0, 0, 0, 0}};
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+    if (c == -1)
+        break;
+    switch (c) {
+        case 0: {
+            switch (option_index) {
+                case 0:
+                    SERV_PORT = atoi(optarg);
+                    if (!(SERV_PORT>0))
+                        return 0;
+                    break;
+                case 1:
+                    BUFSIZE = atoi(optarg);
+                    if (!(BUFSIZE>0))
+                        return 0;
+                    break;
+                case 2:
+                    strcpy(IP, optarg);
+                    if (!(strlen(IP)>0))
+                        return 0;
+                    break;
+                default:
+                    printf("Index %d is out of options\n", option_index);
+            }
+        } break;
+        case '?':
+            printf("Unknown argument\n");
+            break;
+        default:
+            fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+  if (SERV_PORT == -1 || BUFSIZE == -1 || strlen(IP)==0) {
+    fprintf(stderr, "Using: %s --port 20001 --bufsize 4 --ip 127.0.0.1\n", argv[0]);
+    return 1;
+  }
+
+  char  recvline[BUFSIZE + 1];
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket problem");
     exit(1);
   }
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
+  if (inet_pton(AF_INET, IP, &servaddr.sin_addr) < 0) {
+    perror("inet_pton problem ");
+    exit(1);
+  }
+
   servaddr.sin_port = htons(SERV_PORT);
 
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
+  if (connect(sockfd, (SADDR *)&servaddr, SIZE) < 0) {
+    perror("connect problem ");
+    exit(1);
+  }
+
+  //write(1, "Enter string: \n", 13);
+   if (write(sockfd, &BUFSIZE, sizeof(int)) < 0) {
+    perror("write");
+    exit(1);
+  }
+
+  int response = 0;
+  if (read(sockfd, &response, sizeof(int)) < 0) {
+    fprintf(stderr, "Recieve failed\n");
+    exit(1);
+  }
+  printf("PORT %u  \n", response);
+ 
+  int  PORT = response;
+  int fd;
+  struct sockaddr_in servaddr1;
+
+  memset(&servaddr1, 0, sizeof(servaddr1));
+  servaddr1.sin_family = AF_INET;
+  servaddr1.sin_port = htons(PORT);
+
+  if (inet_pton(AF_INET, argv[1], &servaddr1.sin_addr) < 0) {
     perror("inet_pton problem");
     exit(1);
   }
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+  if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
     perror("socket problem");
     exit(1);
   }
-
-  write(1, "Enter string\n", 13);
-
-  while ((n = read(0, sendline, BUFSIZE)) > 0) {
-    if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
-      perror("sendto problem");
+  n=BUFSIZE;
+ 
+  for (int i = 1; i < n + 1; i++) {
+    if (sendto(fd, &i, sizeof(i), 0, (struct sockaddr *)&servaddr1,
+               sizeof(servaddr1)) == -1) {
+      perror("sendto problem1");
       exit(1);
     }
-
-    if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
-      perror("recvfrom problem");
-      exit(1);
-    }
-
-    printf("REPLY FROM SERVER= %s\n", recvline);
   }
+  
+  int i = 0;
+  if (sendto(fd, &i, sizeof(i), 0, (SADDR *)&servaddr1,
+             sizeof(servaddr1)) == -1) {
+    perror("sendto problem2");
+    exit(1);
+  }
+  sleep(1);
+  
+
+    response = -1;
+    int ad = 0;
+
+    while(1) {
+      if (read(sockfd, &response, sizeof(response)) < 0) {
+        fprintf(stderr, "Recieve failed\n");
+        exit(1);
+      }
+     // printf("mi %u  \n", response);
+      if(response == -1) break;
+      if (sendto(fd, &response, sizeof(response), 0, (SADDR *)&servaddr1,
+                 sizeof(servaddr1)) == -1) {
+        perror("sendto problem3");
+        exit(1);
+      }
+    }
+
   close(sockfd);
+  close(fd);
+  exit(0);
+
 }
